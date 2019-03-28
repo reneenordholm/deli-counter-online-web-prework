afc3a0a9437c59c97680c59bc127584353592da9
Renee Nordholm <reneenordholm@gmail.com>
2019-03-28 11:07:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving
+  using_shift
+
+  #using_shift
+  #takes in an argument of an array and uses the shift method to remove the first item and return it
+  #decreases the length of the array by 1
