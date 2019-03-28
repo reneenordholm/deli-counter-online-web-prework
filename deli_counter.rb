dbe13fe64912f0596735667bc68c2734f8fc6727
Renee Nordholm <reneenordholm@gmail.com>
2019-03-28 11:11:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,8 +15,8 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
-  using_shift
+#def now_serving
+#  using_shift
 
   #using_shift
   #takes in an argument of an array and uses the shift method to remove the first item and return it
